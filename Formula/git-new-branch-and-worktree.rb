9d1989a23871b87dcbbcf33988874317b4503a18
Joseph Duffy <commits@josephduffy.co.uk>
2026-02-05 18:39:54 +0000

    Initial commit, pointing to v0.0.1

---
+++ Formula/git-new-branch-and-worktree.rb
@@ -0,0 +1,20 @@
+class GitNewBranchAndWorktree < Formula
+  desc "Create a new git branch and worktree"
+  homepage "https://github.com/JosephDuffy/DuffyUtils"
+  url "https://github.com/JosephDuffy/DuffyUtils/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "07091789008b48fba04685cb9dc1af5771dae78aee33bb4d4ac52e83293366a3"
+  version "0.0.1"
+  head "https://github.com/JosephDuffy/DuffyUtils.git", branch: "main"
+
+  depends_on xcode: ["16.0", :build]
+  depends_on :macos
+
+  def install
+    system "swift", "build", "-c", "release", "--product", "git-new-branch-and-worktree", "--disable-sandbox"
+    bin.install ".build/release/git-new-branch-and-worktree"
+  end
+
+  test do
+    system bin/"git-new-branch-and-worktree", "--help"
+  end
+end
